222623f0aa511b75b05e349480a5cfcccc7f98a5
christopher <cwjg4@cam.ac.uk>
2021-11-28 21:29:57 +0000

    Added functionality

--- fynesse/access.py
+++ fynesse/access.py
@@ -77,7 +77,7 @@ def get_house_prices_by_year_and_county(year, county, conn):
      county, db_id, postcode FROM pp_data
     WHERE date_of_transfer >= '{year}-01-01 00:00:00' 
        AND date_of_transfer < '{year + 1}-01-01 00:00:00'
-       AND county == {county}""")
+       AND county = {county}""")
   row = cur.fetchall()
   cols = ['price', 'date_of_transfer', 'postcode', 'property_type', 'new_build_flag', 'tenure_type', 'locality', 'town_city', 'district', 'county', 'db_id']
   return pd.DataFrame(row, columns=cols)
