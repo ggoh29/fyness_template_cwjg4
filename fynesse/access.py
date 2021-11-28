ced005a63a3d9f3cc2c3b10b3e7e6f11bbedd5e4
christopher <cwjg4@cam.ac.uk>
2021-11-28 22:08:36 +0000

    Added functionality

--- fynesse/access.py
+++ fynesse/access.py
@@ -68,31 +68,19 @@ def get_house_prices(conn):
   cols = ['price', 'date_of_transfer', 'postcode', 'property_type', 'new_build_flag', 'tenure_type', 'locality', 'town_city', 'district', 'county', 'db_id']
   return pd.DataFrame(row, columns=cols)
 
+
 def get_house_prices_by_year_and_county(year, county, conn):
   cur = conn.cursor()
-  string = f'''SELECT price, date_of_transfer, property_type, new_build_flag, 
-     tenure_type, locality, town_city, district, 
+
+  cur.execute(f"""SELECT price, date_of_transfer, property_type, new_build_flag,
+     tenure_type, locality, town_city, district,
      county, db_id, postcode FROM pp_data
-    WHERE county = "{county}"'''
-  print(string)
-  cur.execute(string)
+    WHERE date_of_transfer >= '{year}-01-01 00:00:00'
+       AND date_of_transfer < '{year + 1}-01-01 00:00:00'
+       AND county = '"{county}"'""")
   row = cur.fetchall()
   cols = ['price', 'date_of_transfer', 'postcode', 'property_type', 'new_build_flag', 'tenure_type', 'locality', 'town_city', 'district', 'county', 'db_id']
   return pd.DataFrame(row, columns=cols)
-
-
-# def get_house_prices_by_year_and_county(year, county, conn):
-#   cur = conn.cursor()
-#
-#   cur.execute(f'''SELECT price, date_of_transfer, property_type, new_build_flag,
-#      tenure_type, locality, town_city, district,
-#      county, db_id, postcode FROM pp_data
-#     WHERE date_of_transfer >= '{year}-01-01 00:00:00'
-#        AND date_of_transfer < '{year + 1}-01-01 00:00:00'
-#        AND county = "{county}"''')
-#   row = cur.fetchall()
-#   cols = ['price', 'date_of_transfer', 'postcode', 'property_type', 'new_build_flag', 'tenure_type', 'locality', 'town_city', 'district', 'county', 'db_id']
-#   return pd.DataFrame(row, columns=cols)
 
 
 def data():
