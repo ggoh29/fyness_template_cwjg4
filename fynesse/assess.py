0c6c3b93c9037daa1b1829b3fa498355cd4a24ed
christopher <cwjg4@cam.ac.uk>
2021-12-02 19:44:31 +0000

    Added functionality

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -48,8 +48,7 @@ def bin_price(df):
 
 
 def filter_to_only_data_within_box(df, latitude, longitude, bounds):
-  df["longitude"] = pd.to_numeric(df["longitude"], downcast="float")
-  df["latitude"] = pd.to_numeric(df["latitude"], downcast="float")
+  df[["longitude", 'latitude']] = df[["longitude", 'latitude']].astype(int)
   df = df[(df['longitude'] < float(longitude) + bounds) & (df['longitude'] > float(longitude) - bounds)]
   df = df[(df['latitude'] < float(latitude) + bounds) & (df['latitude'] > float(latitude) - bounds)]
   return df.reset_index(drop = True)
